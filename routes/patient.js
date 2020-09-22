076c2d0f47197f97b1156be868f8e3873e00527e
Mahes Varier <mahesvarier@gmail.com>
2020-09-22 22:42:12 +1000

    GetPatient Endpoint

--- routes/patient.js
+++ routes/patient.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const Patient = require('../model/patient');
+var patientModel = require('../model/patient');
 
-router.post('/create', async (req, res) => {
+router.post('/Create', async (req, res) => {
     const patient = new Patient({
         Name: req.body.name,
         Mobile: req.body.mobile,
@@ -15,6 +16,15 @@ router.post('/create', async (req, res) => {
     } catch (err) {
         res.status(400).send(err);
     }
+})
+
+router.get('/GetPatient', async (req, res) => {
+    console.log('AAAAA', req.body.name);
+    var patientInfo = await patientModel.findOne({ 'Name': req.body.name });
+    console.log(patientInfo);
+    if (patientInfo != null) {
+        res.send(patientInfo)
+    } else { res.status.send('No user found') };
 })
 
 
